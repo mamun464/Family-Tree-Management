6940064c36a119fd71b1d17f3fefa184a3fd80a5
Mamun <mamunurrashid.s.bd@gmail.com>
2024-02-21 16:57:12 +0600

    Deployment Push-3

--- FamilyTree/settings.py
+++ FamilyTree/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@xlvb&btsq7(4g_ferwzat^7up$syguw*)y0^0iq0mib=0k5#l'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
 ALLOWED_HOSTS = []
